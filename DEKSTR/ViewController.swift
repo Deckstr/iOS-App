30d51aefadfa1e06a9569d2e5806be1f9e39fb66
Monte Thakkar <thakkarmanthan9@yahoo.com>
2015-09-13 14:57:54 -0700

    Initial Commit

---
+++ DEKSTR/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DEKSTR
+//
+//  Created by Monte's Pro 13" on 9/13/15.
+//  Copyright (c) 2015 Monte Thakkar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ DEKSTRTests/DEKSTRTests.swift
@@ -0,0 +1,36 @@
+//
+//  DEKSTRTests.swift
+//  DEKSTRTests
+//
+//  Created by Monte's Pro 13" on 9/13/15.
+//  Copyright (c) 2015 Monte Thakkar. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class DEKSTRTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
